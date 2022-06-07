94e85069ef581149191f380c4a1025a70a69a1d8
raphael <raphaelfmiranda@msn.com>
2022-06-07 02:00:00 -0300

    1o teste em C.

--- algoritmoEprogramacaoC/main.c
+++ algoritmoEprogramacaoC/main.c
@@ -31,7 +31,7 @@ int main()
                 int n1, n2;
                 printf("DIGITE DOIS NUMEROS: \n")   ;   scanf("%d %d", &n1, &n2);
                 printf("A SOMA DOS NUMEROS E: %d",  n1 + n2);
-                return 0;
+                break;
             }
 
         case (2):
@@ -39,7 +39,7 @@ int main()
                 float nDias;
                 printf("DIGITE A QUANTIDADE DE DIAS \n")   ;   scanf("%f", &nDias);
                 printf("O VALOR A SER PAGO E: \n%.2f", 50.70 * nDias);
-                return 0;
+                break;
             }
 
         case (3):
@@ -48,7 +48,7 @@ int main()
                 printf("PRECO DO PAO: \n")          ;    scanf("%f", &valorPao);
                 printf("QUANTIDADE DE PAES: \n")    ;    scanf("%f", &qPao);
                 printf("O VALOR A PAGAR E: \n%.2f", valorPao * qPao);
-                return 0;
+                break;
             }
 
         case (4):
@@ -62,14 +62,14 @@ int main()
                 printf("VALOR DO SUCO: \n")           ;       scanf("%f", &valorSuco);
                     total = (qPao * valorPao) + (qLeite * valorLeite) + (qSuco * valorSuco);
                 printf("O VALOR DA COMPRA E: \n%.2f", total);
-                return 0;
+                break;
             }
         case (5):
             {
                 float salarioAtual;
                 printf("VALOR ATUAL DO SALARIO: \n")    ;    scanf("%f", &salarioAtual);
                 printf("O SALARIO REAJUSTADO E: \n%.2f", salarioAtual * 1.25);
-                return 0;
+                break;
             }
         case (6):
             {
@@ -81,7 +81,7 @@ int main()
                 printf("\nOUTRA FORMA | NUMERO AO QUADRADO: %.2f", x * x);
                 printf("\nOUTRA FORMA | NUMERO AO CUBO: %.2f", x * x * x);
                 printf("\nOUTRA FORMA | A RAIZ QUADRADA E: %.2f", x * 1/2);
-                return 0;
+                break;
             }
         case (7):
             {
@@ -89,7 +89,7 @@ int main()
                 printf("DIGITE A SUA IDADE: \n")   ;     scanf("%d", &idade);
                 printf("EM 2018 VOCE TINHA: %d", (idade - (2022 - 2018)));
                 printf("\nEM 2030 VOTE TERA: %d", (idade + (2030 - 2018)));
-                return 0;
+                break;
             }
         case (8):
             {
@@ -100,7 +100,7 @@ int main()
                 perimetro = b * 2 + a * 2;
                 printf("\nO RETANGULO TEM AREA DE: %.2f", area);
                 printf("\nE O PERIMETRO DE: %.2f", perimetro);
-                return 0;
+                break;
             }
             case(9):
             {
@@ -109,7 +109,7 @@ int main()
                 scanf("%f %f", &peso, &altura);
                 imc = peso / pow(altura, 2);
                 printf("\nSEU IMC E: %.2f", imc);
-                return 0;
+                break;
             }
         case (10):
             {
@@ -117,7 +117,7 @@ int main()
                 printf("DIGITE O VALOR DO RAIO: ")      ;   scanf("%f", &r);
                 printf("\nA AREA DO CICULO E: %.2f", (pi * (r * r)));
                 printf("\nA CIRDCUNFERENCIA DO CICULO E: %.2f", (2 * (pi * r)));
-                return 0;
+                break;
             }
         case (11):
             {
@@ -125,7 +125,7 @@ int main()
                 printf("DIGITE QUATRO VALORES NUMERICOS PARA IDADE: \n")    ;   scanf("%f %f %f %f", &i1, &i2, &i3, &i4);
                 iSoma = (i1 + i2 + i3 + i4);
                 printf("A MEDIA E: %.2f", iSoma / 4);
-                return 0;
+                break;
             }
         case (12):
             {
@@ -133,7 +133,7 @@ int main()
                 printf("DIGITE UM NUMERO: ")   ;   scanf("%d", &n);
                 printf("\nSUCESSOR E: %d", n+1);
                 printf("\nANTECESSOR E: %d", n-1);
-                return 0;
+                break;
             }
     }
     return 0;
