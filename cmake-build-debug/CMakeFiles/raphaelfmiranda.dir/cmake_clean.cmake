2a0bc0ab3e91cd15f0266e47694d54467c0b53c9
Raphael <34766364+raphaelfmiranda@users.noreply.github.com>
2022-06-13 23:40:53 -0300

    Create README.md

---
+++ cmake-build-debug/CMakeFiles/raphaelfmiranda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raphaelfmiranda.dir/algoritmoEprogramacaoC/main.c.o"
+  "CMakeFiles/raphaelfmiranda.dir/algoritmoEprogramacaoC/main.c.o.d"
+  "raphaelfmiranda"
+  "raphaelfmiranda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/raphaelfmiranda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
