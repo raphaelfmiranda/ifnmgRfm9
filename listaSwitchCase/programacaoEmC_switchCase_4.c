b98ed2ca29c4c0e159b10eaebc7a976d84e86ef1
Raphael <34766364+raphaelfmiranda@users.noreply.github.com>
2022-06-14 00:32:15 -0300

    Create README.md

--- listaSwitchCase/programacaoEmC_switchCase_4.c
+++ listaSwitchCase/programacaoEmC_switchCase_4.c
@@ -1,38 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
+    int produto, quantidade;
 
-    int codigo_produto , qtd_produto;
-    float valor_final;
+    printf("PRODUTOS DISPONIVEIS:\n");
+    printf("100 - CACHORRO QUENTE\n");
+    printf("101 - BAURU SIMPLES\n");
+    printf("102 - BAURU COM OVO\n");
+    printf("103 - HAMBURGUER\n");
+    printf("104 - CHEESEBURGUER\n");
+    printf("105 - REFRIGERANTE\n");
+    printf("---------------------------------------- \n");
+    printf("DIGITE A SUA OPCAO: ")   ;   scanf("%d", &produto);
+    printf("DIGITE A QUANTIDADE: ")  ;   scanf("%d", &quantidade);
 
-    scanf("%d%d" , &codigo_produto , &qtd_produto);
 
-    switch(codigo_produto){
+    switch (produto)
+    {
+        case (100):
+        {
+            printf("O valor a pagar para %d cachorro quente e: %.2f", quantidade, quantidade * 1.70);
+        }
+        break;
 
-        case 100:
-            valor_final = qtd_produto * 1.70;
-            break;
-        case 101:
-            valor_final = qtd_produto * 2.30;
-            break;
-        case 102:
-            valor_final = qtd_produto * 2.60;
-            break;
-        case 103:
-            valor_final = qtd_produto * 2.40;
-            break;
-        case 104:
-            valor_final = qtd_produto * 2.50;
-            break;
-        case 105:
-            valor_final = qtd_produto * 1.00;
+        case (101):
+        {
+            printf("O valor a pagar para %d bauru simples e: %.2f", quantidade, quantidade * 2.30);
+        }
             break;
 
+        case (102):
+        {
+            printf("O valor a pagar para %d bauru com ovo e: %.2f", quantidade, quantidade * 2.60);
+        }
+            break;
+
+        case (103):
+        {
+            printf("O valor a pagar para %d hamburguer e: %.2f", quantidade, quantidade * 2.40);
+        }
+            break;
+
+        case (104):
+        {
+            printf("O valor a pagar para %d cheeseburguer e: %.2f", quantidade, quantidade * 2.50);
+        }
+            break;
+
+        case (105):
+        {
+            printf("O valor a pagar para %d refrigerante e: %.2f", quantidade, quantidade * 1.00);
+        }
+            break;
+
+        default:
+            printf("VOCE DIGITOU O CODIGO DO PRODUTO ERRADO\n");
     }
-
-    printf("\nValor final :%.2f\n" , valor_final);
-
-
     return 0;
 }
